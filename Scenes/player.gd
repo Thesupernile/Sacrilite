d70687806d955c2094ae177a5fc6f01606145339
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2025-09-28 11:55:11 +0100

    flipping

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,8 +1,22 @@
 extends CharacterBody2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 var speed := 150  # speed in pixels/sec
+enum Direction {
+	LEFT,
+	RIGHT
+}
+var facing = Direction.LEFT
 
 func _physics_process(delta):
 	var direction := Input.get_vector("left", "right", "up", "down")
+	if direction.x > 0:
+		facing = Direction.RIGHT
+		animated_sprite_2d.flip_h = true
+	elif direction.x < 0:
+		facing = Direction.LEFT
+		animated_sprite_2d.flip_h = false
+
+		
 	velocity = direction * speed
 	move_and_slide()
