7040519da33ee7e419555a6ae160bbb54b3296dd
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2025-09-28 18:16:09 +0100

    thingies

--- enemyBase.gd
+++ enemyBase.gd
@@ -3,6 +3,8 @@ class_name enemyBase extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @export var speed := 50
 @export var hp = 5
+var iframes_max = 0.3
+var iframes = 0.0
 
 func _ready():
 	global.connect("enemyHitSignal", enemyHit)
@@ -12,8 +14,9 @@ func enemyDead():
 	self.queue_free()
 
 func enemyHit(damage, nodeHit):
-	if nodeHit == $HurtboxArea:
+	if nodeHit == $HurtboxArea and iframes <= 0.0:
 		hp -= damage
+		iframes = iframes_max
 		if hp <= 0:
 			enemyDead()
 			
