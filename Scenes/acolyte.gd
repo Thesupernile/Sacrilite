d7249000d5326432e386e9c586834592bd9eb970
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2025-09-28 14:22:03 +0100

    acolyte

---
+++ Scenes/acolyte.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@export var hp = 5
+@export var chase_range : float
+
+var speed := 50
+enum Direction {
+	LEFT,
+	RIGHT
+}
+var facing = Direction.LEFT
+
+func _physics_process(_delta):
+	var distance = global.player_position - global_position
+	if distance.length() > chase_range:
+		distance *= 0
+
+	var direction = distance.normalized()
+	if direction.x > 0:
+		facing = Direction.RIGHT
+		animated_sprite_2d.flip_h = true
+	elif direction.x < 0:
+		facing = Direction.LEFT
+		animated_sprite_2d.flip_h = false
+
+	velocity = direction * speed
+	move_and_slide()
