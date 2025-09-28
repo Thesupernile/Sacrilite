b4e7d34f0032dfd27d0bbdadf81f666d3b5ee3ed
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2025-09-28 13:57:14 +0100

    improve cleric

--- Scenes/cleric.gd
+++ Scenes/cleric.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @export var hp = 5
+@export var flee_range = 100
+@export var chase_range = 120
 
 var speed := 50
 enum Direction {
@@ -10,15 +12,19 @@ enum Direction {
 var facing = Direction.LEFT
 
 func _physics_process(_delta):
-	var distance = (global_position - global.player_position)
-	if distance.length() < 100:
-		var direction = distance.normalized()
-		if direction.x > 0:
-			facing = Direction.RIGHT
-			animated_sprite_2d.flip_h = true
-		elif direction.x < 0:
-			facing = Direction.LEFT
-			animated_sprite_2d.flip_h = false
-			
-		velocity = direction * speed
-		move_and_slide()
+	var distance = global.player_position - global_position
+	if distance.length() < flee_range:
+		distance *= -1
+	elif distance.length() < chase_range:
+		distance *= 0
+
+	var direction = distance.normalized()
+	if direction.x > 0:
+		facing = Direction.RIGHT
+		animated_sprite_2d.flip_h = true
+	elif direction.x < 0:
+		facing = Direction.LEFT
+		animated_sprite_2d.flip_h = false
+
+	velocity = direction * speed
+	move_and_slide()
